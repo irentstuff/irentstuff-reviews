764f4e8edb15ab39970866d2d901f6ebe3f75534
Shermin Ho <sherms@Shermins-MacBook-Pro.local>
2024-09-14 11:02:15 +0800

    update data types for item_id, user_id, rental_id

--- reviews/models.py
+++ reviews/models.py
@@ -3,9 +3,9 @@ from uuid import uuid4
 
 class Review(models.Model):
     review_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    rental_id = models.UUIDField(default=uuid4)
-    item_id = models.UUIDField(default=uuid4)
-    user_id = models.UUIDField(default=uuid4)
+    rental_id = models.IntegerField(default=1)
+    item_id = models.IntegerField(default=1) 
+    user_id = models.CharField(max_length=255, default="default_user")
     rating = models.IntegerField(default=5)
     comment = models.TextField(default="Test comment. Happy sunday, funday")
     created_at = models.DateTimeField(auto_now_add=True)
--- reviews/urls.py
+++ reviews/urls.py
@@ -5,12 +5,12 @@ from .views import CreateReview, GetReviewsForItem, GetReviewById, UpdateReview,
 
 urlpatterns = [
     path('reviews/', CreateReview.as_view(), name='create-review'),
-    path('reviews/item/<uuid:item_id>/', GetReviewsForItem.as_view(), name='get-reviews-for-item'),
+    path('reviews/item/<int:item_id>/', GetReviewsForItem.as_view(), name='get-reviews-for-item'),
     path('reviews/<uuid:review_id>/', GetReviewById.as_view(), name='get-review-by-id'),
     path('reviews/update/<uuid:review_id>/', UpdateReview.as_view(), name='update-review'),  # Ensure this line is present
     path('reviews/delete/<uuid:review_id>/', DeleteReview.as_view(), name='delete-review'),
-    path('reviews/user/<uuid:user_id>/', GetReviewsForUser.as_view(), name='get-reviews-for-user'),
-    path('reviews/item/<uuid:item_id>/rating/', GetItemRating.as_view(), name='get-item-rating'),
+    path('reviews/user/<str:user_id>/', GetReviewsForUser.as_view(), name='get-reviews-for-user'),
+    path('reviews/item/<int:item_id>/rating/', GetItemRating.as_view(), name='get-item-rating'),
     # path('test-put/', TestPutView.as_view(), name='test-put'),
     
 ]
